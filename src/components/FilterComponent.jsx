4532e6a74517b9626ccf71d143ee5f7733d7f0e0
kartick17 <karticksadhu0@gmail.com>
2023-12-11 18:49:56 +0530

    Filter add in search room

--- src/components/FilterComponent.jsx
+++ src/components/FilterComponent.jsx
@@ -1,63 +1,41 @@
-const FilterComponent = () => {
+const FilterComponent = ({ premium, single, double, ac, nonAc, changeSingle, changePremium, changeDouble, changeAc, changeNonAc }) => {
   return (
-    <div >
+    <div className="border-4 border-slate-700 px-10 h-96 py-6 mt-48">
 
       {/* <className="bg-white flex flex-col pl-10 pr-20 pt-3.5 pb-12 border-[3px] border-solid border-zinc-300 items-start"> */}
-      <header className="justify-center text-neutral-700 text-center text-3xl font-bold whitespace-nowrap ml-2.5">
+      <header className="justify-center text-stone-800 text-center text-4xl font-bold whitespace-nowrap ml-2.5">
         Filter
       </header>
-      <div className="flex items-stretch gap-2.5 mt-14">
-        <p
-          className="justify-center text-neutral-700 text-2xl font-bold self-center grow whitespace-nowrap my-auto"
-        >
-          Premium
-          <input type="checkbox" name="premium" id="" />
-        </p>
-      </div>
-      <div className="flex items-stretch gap-2.5 mt-14">
-        <p
-          className="justify-center text-neutral-700 text-2xl font-bold self-center grow whitespace-nowrap my-auto"
-        >
-          2-Bed
-          <input type="checkbox" name="2" id="" />
-        </p>
-      </div>
-      <div className="flex items-stretch gap-2.5 mt-14">
-        <p
-          className="justify-center text-neutral-700 text-2xl font-bold self-center grow whitespace-nowrap my-auto"
-        >
-          4-Bed
-          <input type="checkbox" name="4" id="" />
-        </p>
-      </div>
-      <div className="flex items-stretch gap-2.5 mt-14">
-        <p
-          className="justify-center text-neutral-700 text-2xl font-bold self-center grow whitespace-nowrap my-auto"
-        >
-          AC
-          <input type="checkbox" name="ac" id="" />
-        </p>
-      </div>
-      <div className="flex items-stretch gap-2.5 mt-14">
-        <p
-          className="justify-center text-neutral-700 text-2xl font-bold self-center grow whitespace-nowrap my-auto"
-        >
-          Non-AC
-          <input type="checkbox" name="non_ac" id="" />
-        </p>
-      </div>
+      <div className="flex items-center gap-2.5 mt-6 text-2xl font-medium text-stone-800">
 
-      <img
-        loading="lazy"
-        srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/5b6f81d018d45842753660cf6d66a4c1415b43842b8371897fac5c32d4244efe?apiKey=c4cba66cefae4e30bbaa229fca28ec46&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/5b6f81d018d45842753660cf6d66a4c1415b43842b8371897fac5c32d4244efe?apiKey=c4cba66cefae4e30bbaa229fca28ec46&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/5b6f81d018d45842753660cf6d66a4c1415b43842b8371897fac5c32d4244efe?apiKey=c4cba66cefae4e30bbaa229fca28ec46&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/5b6f81d018d45842753660cf6d66a4c1415b43842b8371897fac5c32d4244efe?apiKey=c4cba66cefae4e30bbaa229fca28ec46&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/5b6f81d018d45842753660cf6d66a4c1415b43842b8371897fac5c32d4244efe?apiKey=c4cba66cefae4e30bbaa229fca28ec46&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/5b6f81d018d45842753660cf6d66a4c1415b43842b8371897fac5c32d4244efe?apiKey=c4cba66cefae4e30bbaa229fca28ec46&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/5b6f81d018d45842753660cf6d66a4c1415b43842b8371897fac5c32d4244efe?apiKey=c4cba66cefae4e30bbaa229fca28ec46&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/5b6f81d018d45842753660cf6d66a4c1415b43842b8371897fac5c32d4244efe?apiKey=c4cba66cefae4e30bbaa229fca28ec46&" className="aspect-[1.04] object-contain object-center w-[29px] overflow-hidden shrink-0 max-w-full"
-        alt="Non AC"
-      />
-      <a
-        href="#"
-        className="justify-center text-neutral-700 text-2xl font-bold my-auto"
-      >
-        Non AC
-      </a>
+        Premium
+        <input className="m-1" type="checkbox" name="premium" id="" checked={premium} onChange={changePremium} />
+
+      </div>
+      <div className="flex items-center gap-2.5 mt-6 text-2xl font-medium text-stone-800">
+
+        Single Bed
+        <input className="m-1" type="checkbox" name="2" id="" checked={single} onChange={changeSingle} />
+
+      </div>
+      <div className="flex items-center gap-2.5 mt-6 text-2xl font-medium text-stone-800">
+
+        Double Bed
+        <input className="m-1" type="checkbox" name="4" id="" checked={double} onChange={changeDouble} />
+
+      </div>
+      <div className="flex items-center gap-2.5 mt-6 text-2xl font-medium text-stone-800">
+
+        AC
+        <input className="m-1" type="checkbox" name="ac" id="" checked={ac} onChange={changeAc} />
+
+      </div>
+      <div className="flex items-center gap-2.5 mt-6 text-2xl font-medium text-stone-800">
+
+        Non-AC
+        <input className="m-1" type="checkbox" name="non_ac" id="" checked={nonAc} onChange={changeNonAc} />
+
+      </div>
     </div>
 
   )
--- src/pages/HotelList.jsx
+++ src/pages/HotelList.jsx
@@ -3,11 +3,39 @@ import HotelListCard from "../components/HotelListCard";
 import axios from "axios";
 import { BASE_URL } from "../utils/constant";
 import { useParams } from "react-router-dom";
+import FilterComponent from "../components/FilterComponent";
 
 const HotelList = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState();
   const [rooms, setRooms] = useState();
+
+  const [premium, setPremium] = useState(false)
+  const [single, setSingle] = useState(false)
+  const [double, setDouble] = useState(false)
+  const [ac, setAc] = useState(false)
+  const [nonAc, setNonAc] = useState(false)
+
+
+  const changePremium = () => {
+    setPremium(pre => !pre)
+  };
+
+  const changeSingle = () => {
+    setSingle(pre => !pre)
+  }
+
+  const changeDouble = () => {
+    setDouble(pre => !pre)
+  }
+
+  const changeAc = () => {
+    setAc(pre => !pre)
+  }
+
+  const changeNonAc = () => {
+    setNonAc(pre => !pre)
+  }
 
   const { location, checkinDate, checkoutDate } = useParams();
   let url = `${BASE_URL}/rooms/all-rooms/?`
@@ -16,12 +44,18 @@ const HotelList = () => {
     url += `location=${location}`
   }
 
+  if (premium) url += `&category=premium`
+  if (single) url += `&bed_type=single`
+  if (double) url += `&bed_type=double`
+  if (ac) url += `&room_type=ac`
+  if (nonAc) url += `&room_type=non-ac`
+
   useEffect(() => {
     getAllRooms();
-  }, []);
+  }, [premium, single, double, ac, nonAc]);
 
   const getAllRooms = async () => {
-    // console.log(url)
+    console.log(url)
     try {
       setIsLoading(true);
       const res = await axios.get(url);
@@ -39,8 +73,8 @@ const HotelList = () => {
 
   return (
     <div className="mt-10 flex justify-around">
-      <div>filter</div>
-      <div className="flex flex-col gap-4">
+      <FilterComponent premium={premium} single={single} double={double} ac={ac} nonAc={nonAc} changeSingle={changeSingle} changePremium={changePremium} changeDouble={changeDouble} changeAc={changeAc} changeNonAc={changeNonAc} />
+      <div className="flex flex-col gap-6">
         {rooms.map((room) => (
           <HotelListCard key={room.id} room={room} checkinDate={checkinDate} checkoutDate={checkoutDate} />
         ))}
